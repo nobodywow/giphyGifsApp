7a38707c83dd23cb87ffd122f50d60c31865d857
Anton K <ntnkonon@gmail.com>
2019-06-03 20:31:07 +0300

    cfg fix

--- src/cfg/env.js
+++ src/cfg/env.js
@@ -1,5 +1,5 @@
 export const env = {
     API_KEY: "GJ3cFTvnDd8Qu2U0fB5feCKamyvnrarm",
     BASE_URL: "https://api.giphy.com/v1/gifs/",
-    PATH_SUFFIX: 'giphyGifsApp',
+    PATH_SUFFIX: '/giphyGifsApp',
 };
